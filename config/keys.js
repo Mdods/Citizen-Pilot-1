355d427c90532892726ee3bb4000d9f36b385454
Mdods <mattdodsworth@yahoo.co.uk>
2021-04-03 11:16:55 +0100

    read me update

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyBWBEU3-rZW9jy5TyfCRKudCDvyRr05xfQ",
+    authDomain: "citizen-pilot.firebaseapp.com",
+    projectId: "citizen-pilot",
+    storageBucket: "citizen-pilot.appspot.com",
+    messagingSenderId: "194970765947",
+    appId: "1:194970765947:web:d14f04a2d796473855f0de"
+  }
+};
